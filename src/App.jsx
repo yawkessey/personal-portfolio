32a57757405c4e5ad06cfe9d4a2a366f67d4c144
yawkessey <yawkesseyankomah@gmail.com>
2022-09-18 01:25:29 -0400

    Imported components into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import Header from './components/Header/Header'
+import Nav from './components/Nav/Nav'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Experience from './components/Experience/Experience'
+import Footer from './components/Footer/Footer'
+import Portfolio from './components/Portfolio/Portfolio'
+import Services from './components/Services/Services'
+import Testimonials from './components/Testimonials/Testimonials'
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   )
 }
